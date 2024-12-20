2f9955a5842344614abed77bd7e5f3b821a57c48
Ameema Saleem <161148265+ameemasaleem@users.noreply.github.com>
2024-12-20 21:31:35 +0500

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
+// module.exports = {
+//   swcMinify: true,
+//   experimental: {
+//     swcLoader: true,
+//     swcMinify: true,
+//   },
+// };
